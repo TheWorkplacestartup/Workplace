9fcb0904beb95455903436b06ddb43d5a69158b1
Djibril Keita <djibril@TheWorkplacestartup.com>
2013-11-30 13:46:55 -0500

    bloah

--- public/index.php
+++ public/index.php
@@ -1,10 +1,21 @@
 <?php
 // Controler
-
+session_start();
+$_SESSION['loggedin'] = 0;
+if ($_SESSION ['loggedin'] == 1) {
+    // logged in
+    echo "You are logged in ";
+} else {
+    // Not logged  in 
+    echo 'You are Not logged in ' ;
+    echo file_get_contents ("../templates/signinform.html");
+    
+}
     if ($_SERVER['REQUEST_URI']== '/'){
-	echo file_get_contents ("../templates/index.html");
+	
 	//Homepage
 	} else{
+
 	//Not Homepage
 }
 
